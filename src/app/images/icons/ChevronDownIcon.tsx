50b80b659a575a7ac50a2e118d593802e1b7e010
Marcos Vinicius <marcos.v.silva@live.com>
2023-09-19 22:07:05 +0100

    feat: adding icons

---
+++ src/app/images/icons/ChevronDownIcon.tsx
@@ -0,0 +1,13 @@
+function ChevronDownIcon({ ...props }) {
+  return (
+    <svg
+      viewBox="0 0 8 6"
+      aria-hidden="true"
+      {...props}
+    >
+      {/* SVG path content */}
+    </svg>
+  );
+}
+
+export default ChevronDownIcon;
---
+++ src/app/images/icons/SunIcon.tsx
@@ -0,0 +1,16 @@
+function SunIcon({ ...props }) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+      {...props}
+    >
+      {/* SVG path content */}
+    </svg>
+  );
+}
+
+export default SunIcon;
