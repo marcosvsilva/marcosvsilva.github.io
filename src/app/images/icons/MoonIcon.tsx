afe1020cff2f9fba2bfa916c22685bd7ee6a5f7a
Marcos Vinicius <marcos.v.silva@live.com>
2023-09-20 01:00:29 +0100

    feat: adding more icons

---
+++ src/app/images/icons/MoonIcon.tsx
@@ -0,0 +1,13 @@
+function MoonIcon({ ...props }) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+      {...props}
+    >
+      {/* SVG path content */}
+    </svg>
+  );
+}
+
+export default MoonIcon;
