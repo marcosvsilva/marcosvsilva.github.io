8afa0817f29c5a93bcb684f2ed851d0008b196bc
Marcos Vinicius <marcos.v.silva@live.com>
2023-09-25 23:49:27 +0100

    fix: fixing style issues

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -10,20 +10,21 @@ type AvatarProps = {
   style?: React.CSSProperties;
 };
 
-function Avatar({ large = false, className, style }: AvatarProps) {
+function Avatar({ large = false, className, style, ...props }: AvatarProps) {
   return (
     <Link
       href="/"
       aria-label="Home"
       className={clsx(className, 'pointer-events-auto')}
+      {...props}
     >
       <Image
         src={avatarImage}
         alt="Marcos Silva"
-        width={large ? 64 : 36}
-        height={large ? 64 : 36}
+        sizes={large ? ('4rem' as string) : ('2.25rem' as string)}
         style={style}
         className={clsx('rounded-full bg-zinc-100 object-cover dark:bg-zinc-800', large ? 'h-16 w-16' : 'h-9 w-9')}
+        priority
       />
     </Link>
   );
