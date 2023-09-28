81acb79ac281b7e1b8fa7d5d6c17e1f4962f5eca
Marcos Vinicius <marcos.v.silva@live.com>
2023-09-28 02:11:21 +0100

    feat: adding article to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Article from './components/Article';
 import Container from './components/Container';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -60,7 +61,10 @@ export default function Home() {
           </Container>
           <Container className="mt-24 md:mt-28">
             <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-              <div className="space-y-10 lg:pl-16 xl:pl-24">
+              <div className="flex flex-col gap-16">
+                <Article />
+              </div>
+              <div className="space-y-10 lg:pl-16 xl:pl-18">
                 <Resume />
               </div>
             </div>
