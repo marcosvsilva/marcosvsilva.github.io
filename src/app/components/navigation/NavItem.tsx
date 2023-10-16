21e56b199f952d3c9f3270d317920781b487b737
Marcos Vinicius <marcos.v.silva@live.com>
2023-10-16 21:22:09 +0100

    fix: improving navitem component style

--- src/app/components/navigation/NavItem.tsx
+++ src/app/components/navigation/NavItem.tsx
@@ -1,5 +1,6 @@
+import clsx from 'clsx';
 import Link from 'next/link';
-import React from 'react';
+import { usePathname } from 'next/navigation';
 
 type NavLinkProps = {
   link: string;
@@ -7,9 +8,19 @@ type NavLinkProps = {
 };
 
 function NavItem({ link, children }: NavLinkProps) {
+  const pathname = usePathname();
+  const isActive = pathname === link;
+
   return (
     <Link href={link}>
-      <div className="transition hover:text-teal-500 dark:hover:text-teal-400">{children}</div>
+      <div
+        className={clsx('relative block px-3 py-2 transition', isActive ? 'text-teal-500 dark:text-teal-400' : 'hover:text-teal-500 dark:hover:text-teal-400')}
+      >
+        {children}
+        {isActive && (
+          <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
+        )}
+      </div>
     </Link>
   );
 }
